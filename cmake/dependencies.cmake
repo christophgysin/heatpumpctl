841b5823a08543bc73c991d7027fde950a2e5e4f
Christoph Gysin <christoph.gysin@gmail.com>
2012-10-30 22:32:24 +0200

    add serial classes

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,2 +1,4 @@
 include(FindBoost)
-find_package(Boost REQUIRED system filesystem program_options thread)
+set(BOOST_LIBS thread date_time system filesystem program_options)
+find_package(Boost COMPONENTS ${BOOST_LIBS} REQUIRED)
+find_package(Threads REQUIRED)
--- src/heatpumpctl.cpp
+++ src/heatpumpctl.cpp
@@ -1,6 +1,14 @@
-#include <boost/asio.hpp>
+#include <iostream>
+#include <serial/BufferedAsyncSerial.h>
 
 int main(int, char**)
 {
+    BufferedAsyncSerial serial("/dev/ttyUSB0", 56700);
+    serial.writeString("1800\r\n");
+
+    std::string response = serial.readStringUntil("\r\n");
+
+    std::cout << response << std::endl;
+
     return EXIT_SUCCESS;
 }
